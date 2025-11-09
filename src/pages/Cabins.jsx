4aaac330f52c5ec141f0ceb0af2904cb43793294
precious lan <olanrewaju.faj@gmail.com>
2025-11-09 13:16:24 +0100

    new

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
+
+function Cabins() {
+
+ 
+
+  return (
+    <>
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>filter/sort</p>
+    </Row>
+
+    <Row>
+      <CabinTable/>
+    </Row>
+    
+    </>
+  );
+}
+
+export default Cabins;
